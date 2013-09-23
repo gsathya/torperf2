e6c6cffc719ef3eeb68a4e7fa39679e902c692af
Kevin Butler <haqkrs@gmail.com>
2013-09-23 05:39:43 +0100

    Fixed shebang

--- torperf.py
+++ torperf.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
+#
 # Copyright (c) 2013, Sathyanarayanan Gunasekaran, The Tor Project, Inc.
 # See LICENSE for licensing information
-#!/usr/bin/env python
 
 import perfconf
 
@@ -9,7 +10,7 @@ from torperf.core.experimentScheduler import ExperimentScheduler
 
 from twisted.internet import reactor, task
 
-file_server = TorPerfFileServer(reactor, perfconf.tor_config);
+file_server = TorPerfFileServer(reactor, perfconf.tor_config)
 file_server.startServer()
 
 experimenTor = ExperimentScheduler(reactor, perfconf.tor_config)
