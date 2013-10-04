c36bae59ec603211cf2e014fea3dcfed8c296576
Kevin Butler <haqkrs@gmail.com>
2013-10-04 05:56:08 +0100

    add "source" to server config

--- perfconf.py
+++ perfconf.py
@@ -2,6 +2,7 @@
 # See LICENSE for licensing information
 
 tor_config = {
+	'source': 'server_identifer',
     'socks_port' : 9050,
     'http_port' : 8888,
     'http_host' : 'http://torperf2.ngrok.com',
