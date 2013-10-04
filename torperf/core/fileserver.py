7710d4a7af7d3a91537a576fea7ad89346699ff3
Kevin Butler <haqkrs@gmail.com>
2013-10-04 05:19:46 +0100

    Added timings datastore to fileserver

--- torperf/core/fileserver.py
+++ torperf/core/fileserver.py
@@ -5,45 +5,54 @@ from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.web import resource
 from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet import interfaces
 
 class TimedFile(File):
-    counter = 0
-    processStartTime = time.time()
-    @classmethod
-    def getUniqueId(cls):
-        # This should be safe as twisted is single threaded?
-        # It's also using the process start time so there's no
-        # collisions if the service gets rebooted
-        cls.counter += 1
-        return "%s-%s" % (cls.processStartTime, cls.counter)
+    def __init__(self, server, path, defaultType="text/html", ignoredExts=(), registry=None, allowExt=0):
+        File.__init__(self, path, defaultType, ignoredExts, registry, allowExt)
+        self.server = server
 
     def render_GET(self, request):
-        uniqueRequestId = TimedFile.getUniqueId()
-        
-        # Log first byte time
-        self.logFirstByte(uniqueRequestId)
+        req_headers = dict(request.getAllHeaders())
 
-        # Give the client a unique identifier
-        request.setHeader('X-Torperf-request-id', uniqueRequestId)
+        # Didn't come from our client
+        if not 'x-torperfproxyid' in req_headers:
+            return File.render_GET(self, request)
+
+        unique_id = req_headers['x-torperfproxyid']
+
+        # Create an entry in our datastore
+        times = {}
+        self.server.datastore[unique_id] = times
+        self.timer = interfaces.IReactorTime(self.server.reactor)
+
+        # Log first byte time
+        times['SERVER_FIRSTBYTE'] = "%0.2f" % self.timer.seconds()
 
         # Log last byte time when the request finishes
-        request.notifyFinish().addCallback(self.logLastByte, uniqueRequestId)
+        request.notifyFinish().addCallback(self.logLastByte, times)
         return File.render_GET(self, request)
 
-    def logFirstByte(self, uniqueRequestId):
-        print "Starting request for %s at %s" % (uniqueRequestId, time.time())
-
-    def logLastByte(self, error, uniqueRequestId):
+    def logLastByte(self, error, times):
         if error:
             # Request closed early, should log the error
-            pass
+            times['SERVER_ERROR'] = str(error)
 
-        # Log last byte time
-        print "Finished request for %s at %s" % (uniqueRequestId, time.time())
+        times['SERVER_LASTBYTE'] = "%0.2f" % self.timer.seconds()
+
+    # Probably better ways to handle this
+    def createSimilarFile(self, path):
+        f = self.__class__(self.server, path, self.defaultType, self.ignoredExts, self.registry)
+        # refactoring by steps, here - constructor should almost certainly take these
+        f.processors = self.processors
+        f.indexNames = self.indexNames[:]
+        f.childNotFound = self.childNotFound
+        return f
 
 class FileDispatcher(resource.Resource):
     children = {}
-    def __init__(self, server_config):
+    def __init__(self, server, server_config):
+        self.server = server
         self._experiments_dir = server_config['experiments_dir']
         
         for dir in os.listdir(self._experiments_dir):
@@ -52,19 +61,31 @@ class FileDispatcher(resource.Resource):
 
     def addExperiment(self, name):
         servePath = self._experiments_dir + name + "/public"
-        self.putChild(name, TimedFile(servePath))
+        self.putChild(name, TimedFile(self.server, servePath))
         print "Added static files for: %s" % name
 
 class TorPerfFileServer(object):
     def __init__(self, reactor, server_config):
-        self._reactor = reactor
+        self.reactor = reactor
+        self.datastore = {}
         self._config = server_config
-        self._dispatcher = FileDispatcher(self._config)
+        self._dispatcher = FileDispatcher(self, self._config)
 
     def startServer(self):
-        self.web_endpoint = TCP4ServerEndpoint(self._reactor,
+        self.web_endpoint = TCP4ServerEndpoint(self.reactor,
                                                 self._config['http_port'])
         root = resource.Resource()
         root.putChild("static", self._dispatcher)
+
         self.web_endpoint.listen(Site(root))
         print "Http Server started."
+
+    def get_timings(self, identifier):
+        if identifier in self._datastore:
+            result = self.datastore[identifier]
+            # Remove from results set
+            del self.datastore[identifier]
+            return result
+        else:
+            print "No timings found for %s" % identifier
+            return None
